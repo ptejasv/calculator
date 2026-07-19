b3a9a4d212e2678ad1a9ae61abdc1403db0730d4
ptejasv <e0564194@u.nus.edu>
2026-07-19 20:45:02 +0800

    feat: add basic result calculation for 2 numbers

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,13 @@
-OPERATOR_ADD = "+"
-OPERATOR_SUBTRACT = "-"
-OPERATOR_MULTIPLY = "*"
-OPERATOR_DIVIDE = "/"
+let currentNum = 0; // current number being updated: num0 or num1
+let num0 = num1 = 0; // values of the 2 numbers to operate on
+let operator = null; 
+
+const OPERATORS = {
+    "buttonDivide": "OPERATOR_DIVIDE",
+    "buttonTimes": "OPERATOR_MULTIPLY",
+    "buttonMinus": "OPERATOR_SUBTRACT",
+    "buttonPlus": "OPERATOR_ADD"
+}
 
 // basic math operators
 function add(a, b) {
@@ -31,13 +37,89 @@ function operate(a, b, operator) {
      * @return {Number} Returns the value of the operation
      */
     switch (operator) {
-        case OPERATOR_ADD:
+        case "OPERATOR_ADD":
             return add(a, b);
-        case OPERATOR_SUBTRACT:
+        case "OPERATOR_SUBTRACT":
             return subtract(a, b);
-        case OPERATOR_MULTIPLY:
+        case "OPERATOR_MULTIPLY":
             return multiply(a, b);
-        case OPERATOR_DIVIDE:
+        case "OPERATOR_DIVIDE":
             return divide(a, b);
     }
 }
+
+const display = document.querySelector(".display p");
+const buttons = document.querySelectorAll(".calc-buttons");
+
+function addToDisplay(text) {
+    display.textContent = display.textContent + text;
+}
+
+function handleDigitBtn(btnText) {
+    /**
+     * Callback function for digit button press. Updates either num0 or num1 
+     * based on the value of currentNum. Displays the updated number in the 
+     * calculator console.
+     * 
+     * @method handleDigitBtn
+     * @param {String} btnText Text content of the button that was pressed
+     */
+    let digit = parseInt(btnText);
+    
+    if (currentNum == 0) num0 = (num0 * 10) + digit;
+    else num1 = (num1 * 10) + digit;
+
+    addToDisplay(digit);
+}
+
+function handleOpBtn(btn) {
+    /**
+     * Callback function for operator button presses. Operators can be AC, 
+     * equals, add, subtract, times or divide.
+     * 
+     * @method handleOpBtn
+     * @param {Element} btn The button node that was clicked
+     */
+    const btnId = btn.getAttribute("id");
+
+    switch (btnId) {
+        case "buttonAC":
+            // reset globals
+            currentNum = 0;
+            num0 = num1 = 0;
+            operator = null;
+
+            // clear text
+            display.textContent = "";
+            break;
+        case "buttonEquals":
+            // get result
+            let result = operate(num0, num1, operator);
+            display.textContent = result;
+
+            // reset globals
+            currentNum = 0;
+            num0 = num1 = 0;
+            operator = null;
+
+            break;
+        default:
+            operator = OPERATORS[btnId];
+            addToDisplay(` ${btn.textContent} `);
+
+            // swap current num being updated
+            currentNum = !currentNum;
+    }
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (e.target.classList.contains("btn-digit")) {
+            // digit button pressed
+            handleDigitBtn(e.target.textContent);
+        } else if (e.target.classList.contains("btn-op")) {
+            // operator button pressed
+            handleOpBtn(e.target);
+        }
+    })
+})
